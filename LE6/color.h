edabbe97fb4a90bc16f0b5824fae253696c3279b
Vicente-RR <vicente.rodriguez.rapetti@gmail.com>
2025-05-07 19:56:54 -0300

    Create color.h

---
+++ LE6/color.h
@@ -0,0 +1,29 @@
+//Esta biblioteca permite modificar la apariencia del texto con printf
+
+#include <stdio.h>
+
+#define RESET "\033[0m"
+
+#define NEGRO "\033[30m"
+#define ROJO "\033[31m"
+#define VERDE "\033[32m"
+#define AMARILLO "\033[33m"
+#define AZUL "\033[34m"
+#define VIOLETA "\033[35m"
+#define CELESTE "\033[36m"
+#define BLANCO "\033[37m"
+
+#define NEGRITA "\033[1m"
+#define DESVANECIDO "\033[2m"
+#define CURSIVA "\033[3m"
+#define SUBRAYADO "\033[4m"
+#define NEGATIVO "\033[7m"
+
+#define F_NEGRO "\033[40m"
+#define F_ROJO "\033[41m"
+#define F_VERDE "\033[42m"
+#define F_AMARILLO "\033[43m"
+#define F_AZUL "\033[44m"
+#define F_VIOLETA "\033[45m"
+#define F_CELESTE "\033[46m"
+#define F_BLANCO "\033[47m"
