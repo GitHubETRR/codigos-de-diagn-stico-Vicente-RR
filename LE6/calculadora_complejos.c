7bb7a906280bcb820ff36726ab46396aa8456980
Vicente-RR <vicente.rodriguez.rapetti@gmail.com>
2025-08-22 14:53:24 -0300

    Create calculadora_complejos.c

---
+++ LE6/calculadora_complejos.c
@@ -0,0 +1,176 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+#define CARTESIANA '1'
+#define POLAR '2'
+
+typedef enum
+{
+    CARGAR_A = 1,
+    CARGAR_B,
+    SUMAR,
+    RESTAR,
+    MULTIPLICAR,
+    DIVIDIR
+}opciones_t;
+
+class complejo
+{
+    private:
+        float x, y;
+    public:
+        complejo();
+        void Guarda(float x2, float y2);
+        void Cartesiana(float &x2, float &y2);
+        void Polar(float &mod, float &ang);
+}
+
+complejo::complejo()
+{
+    x = 0;
+    y = 0;
+}
+
+void complejo::Guarda(float x2, float y2)
+{
+    x = x2;
+    y = y2;
+}
+
+void complejo::Cartesiana(float &x2, float &y2)
+{
+    x2 = x;
+    y2 = y;
+}
+
+void complejo::Polar(float &mod, float &ang)
+{
+    mod = sqrt(x*x + y*y);
+    ang = atan(y/x);
+}
+
+opciones_t Menu();
+complejo PedirDatos();
+bool PedirForma();
+void LimpiarPantalla();
+
+int main()
+{
+    new complejo a;
+    new complejo b;
+    
+    do
+    {
+        opciones_t opcion = Menu();
+        switch (opcion)
+        {
+            case CARGAR_A:
+            a = PedirDatos();
+            break;
+            
+            case CARGAR_B:
+            break;
+            
+            case SUMAR:
+            break;
+            
+            case RESTAR:
+            break;
+            
+            case MULTIPLICAR:
+            break;
+            
+            case DIVIDIR:
+            break;
+            
+            default:
+            salir = true;
+            break;
+        }
+    } while (!salir);
+    
+    delete a;
+    delete b;
+
+    return 0;
+}
+
+opciones_t Menu()
+{
+    opciones_t opcion;
+    cout << "Calculadora de números complejos" << endl;
+    cout << CARGAR_A << ". Cargar el complejo A" << endl;
+    cout << CARGAR_B << ". Cargar el complejo B" << endl;
+    cout << SUMAR << ". Sumar complejos A+B" << endl;
+    cout << RESTAR << ". Restar complejos A-B" << endl;
+    cout << MULTIPLICAR << ". Multiplicar complejos A*B" << endl;
+    cout << DIVIDIR << ". Dividir complejos A/B" << endl;
+    cout << "Otra tecla para salir" << endl;
+    cin >> opcion;
+    LimpiarPantalla();
+    return opcion;
+}
+
+complejo PedirDatos()
+{
+    complejo z;
+    bool formaCartesiana = PedirForma();
+    if (formaCartesiana)
+        PedirCartesiana(z);
+    else
+        PedirPolar(z);
+    return z;
+}
+
+bool PedirForma()
+{
+    char opcion;
+    bool opcionValida = false;
+    bool formaCartesiana;
+    
+    cout << "Enviá " << CARTESIANA << " para ingresar en forma cartesiana" << endl;
+    cout << "Enviá " << POLAR << " para ingresar en forma polar" << endl;
+    do
+    {
+        cin >> opcion;
+        if ((opcion == CARTESIANA) || (opcion == POLAR))
+            opcionValida = true;
+        else
+            cout << "Opción no válida" << endl;
+    } while (!opcionValida);
+    if (opcion == CARTESIANA)
+        formaCartesiana = true;
+    else
+        formaCartesiana = false;
+    LimpiarPantalla();
+    
+    return formaCartesiana;
+}
+
+void PedirCartesiana(complejo &z)
+{
+    float x, y;
+    cout << "Ingresa la parte real" << endl;
+    cin >> x;
+    cout << "Ingresa la parte imaginaria" << endl;
+    cin >> y;
+    z.Guarda(x, y);
+}
+
+void PedirPolar(complejo &z)
+{
+    float mod, ang, x, y;
+    
+}
+
+void LimpiarPantalla()
+{
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
